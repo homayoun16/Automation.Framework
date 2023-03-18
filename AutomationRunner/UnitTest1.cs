c2d54d1a2ae8a9614757cbb6ed002c96f4814219
homayoun16 <h.samavatian@hotmail.com>
2023-03-17 18:53:00 -0700

    no message

---
+++ AutomationRunner/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AutomationRunner;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+        // this is a test
+    }
+}
